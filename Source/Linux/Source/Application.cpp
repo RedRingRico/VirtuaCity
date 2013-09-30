517c892bc4f47686407f3e229560025293c193d3
Rico <teh_programerer@interactivelaboratories.com>
2013-09-30 16:37:26 +0100

    |SOURCE| * Stubbed-out all member functions

--- Source/Linux/Source/Application.cpp
+++ Source/Linux/Source/Application.cpp
@@ -1,2 +1,31 @@
 #include <Application.hpp>
 
+namespace VirtuaCity
+{
+	Application::Application( )
+	{
+	}
+
+	Application::~Application( )
+	{
+	}
+
+	ZED_UINT32 Application::Initialise( )
+	{
+		return ZED_OK;
+	}
+
+	ZED_UINT32 Application::Execute( )
+	{
+		return ZED_OK;
+	}
+
+	void Application::Update( )
+	{
+	}
+
+	void Application::Render( )
+	{
+	}
+}
+
